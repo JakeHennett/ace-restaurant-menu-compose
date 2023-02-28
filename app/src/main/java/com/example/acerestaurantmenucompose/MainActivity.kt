ed3d1c5c2d4c2217c89ab806a8609bf2cfb1914a
Jake Hennett <jake.hennett@gmail.com>
2023-02-28 14:09:11 -0500

    Added a Modifier with padding. Cannot get color working. Missing import?

--- app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
+++ app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
@@ -4,7 +4,9 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.acerestaurantmenucompose.ui.theme.AceRestaurantMenuComposeTheme
 
@@ -36,7 +39,10 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        fontSize = 30.sp
+        fontSize = 30.sp,
+        modifier = Modifier
+            .padding(16.dp)
+            //.background(Color.Red)
     )
 }
 
