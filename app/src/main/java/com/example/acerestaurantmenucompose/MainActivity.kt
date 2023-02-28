450f2d34c2f98b5fd2805e20bff30436720946d1
Jake Hennett <jake.hennett@gmail.com>
2023-02-28 14:41:38 -0500

    Finished formatting section of tutorial

--- app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
+++ app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
@@ -5,13 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    PlayWithFormatting("Android")
                 }
             }
         }
@@ -37,8 +36,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column {
+fun PlayWithFormatting(name: String) {
+    Box(
+        modifier = Modifier
+            .size(400.dp),
+    ) {
         Text(
             text = "Hello $name!",
             fontSize = 30.sp,
@@ -48,7 +50,9 @@ fun Greeting(name: String) {
                 .padding(16.dp)
         )
         Text(
-            text = "A Second Row"
+            text = "Bottom Right Button",
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
         )
 
     }
