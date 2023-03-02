6b5540660a9698f02c877ab34ea7554d86f150d5
Jake Hennett <jake.hennett@gmail.com>
2023-03-01 19:40:34 -0500

    Verified issues with DefaultView were corrected

--- app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
+++ app/src/main/java/com/example/acerestaurantmenucompose/MainActivity.kt
@@ -9,24 +9,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.acerestaurantmenucompose.ui.theme.AceRestaurantMenuComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -71,7 +66,7 @@ fun MainMenu()
                 .fillMaxWidth()
         ){
             var menuItems = ArrayList<String>()
-            PopulateMenuItems(menuItems)
+            populateMenuItems(menuItems)
             MenuList(menuItems)
         }
     }
@@ -140,7 +135,7 @@ fun DefaultPreview() {
     }
 }
 
-fun PopulateMenuItems(arrayList: ArrayList<String>)
+fun populateMenuItems(arrayList: ArrayList<String>)
 {
     //TODO: use this subroutine to fetch JSON from gist
     arrayList.add("Menu")
@@ -152,6 +147,7 @@ fun PopulateMenuItems(arrayList: ArrayList<String>)
     arrayList.add("Get Directions")
     arrayList.add("About Us")
     arrayList.add("Contact Us")
+    arrayList.add("Debug")
 }
 
 fun menuItemClicked(name: String, context: Context)
